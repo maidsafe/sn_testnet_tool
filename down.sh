c4518cc960bd5e8f025adc6f086fc227199afb8b
Josh Wilson <joshuef@gmail.com>
2021-06-14 09:37:43 +0200

    chore: increase wait, more chunks and one more loop of churn

--- down.sh
+++ down.sh
@@ -1,5 +1,6 @@
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 TESTNET_CHANNEL=$(terraform workspace show)
+AUTO_APPROVE=${2}
 
-terraform destroy -var "do_token=${DO_PAT}" -var "pvt_key=${1}"  --parallelism 15 && \
+terraform destroy -var "do_token=${DO_PAT}" -var "pvt_key=${1}"  --parallelism 15 ${AUTO_APPROVE} && \
     rm ${TESTNET_CHANNEL}-ip-list || true
