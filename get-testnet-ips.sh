80106ca5c318cf25901b3f2b6bca2e3de3013af0
Josh Wilson <joshuef@gmail.com>
2021-12-15 19:04:39 +0100

    feat: add script to get current workspace data from aws

---
+++ get-testnet-ips.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+DEFAULT_WORKING_DIR="."
+WORKING_DIR="${WORKING_DIR:-$DEFAULT_WORKING_DIR}"
+
+testnet_channel=$(terraform workspace show)
+
+
+
+function copy_ips_from_s3() {
+    aws s3 cp \
+        "s3://safe-testnet-tool/$testnet_channel-ip-list" \
+        "$WORKING_DIR/$testnet_channel-ip-list" 
+    aws s3 cp \
+        "s3://safe-testnet-tool/$testnet_channel-genesis-ip" \
+        "$WORKING_DIR/$testnet_channel-genesis-ip" 
+    aws s3 cp \
+        "s3://safe-testnet-tool/$testnet_channel-node_connection_info.config" \
+        "$WORKING_DIR/$testnet_channel-node_connection_info.config" 
+}
+
+
+copy_ips_from_s3
+./scripts/register_keys.sh
