9140eed16b992f738878a41d0d9d86a5154f468b
Lionel Faber <lionel1704@gmail.com>
2022-01-10 19:05:54 +0530

    fix(build): add env variable to prevent file-lock failure for apt-get install commands

--- build.tf
+++ build.tf
@@ -14,43 +14,16 @@ resource "digitalocean_droplet" "node_builder" {
         timeout = "10m"
         private_key = file(var.pvt_key)
     }
-
     provisioner "remote-exec" {
         inline = [
-           "apt-get update",
-            "apt-get install build-essential -y",
-            # "bash",
-            <<-EOT
-                while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-                    sleep 1
-                done
-                while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1 ; do
-                    sleep 1
-                done
-                while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
-                    sleep 1
-                done
-                while sudo fuser /var/lib/apt/lists/ >/dev/null 2>&1 ; do
-                    sleep 1
-                done
-                if [ -f /var/log/unattended-upgrades/unattended-upgrades.log ]; then
-                    while sudo fuser /var/log/unattended-upgrades/unattended-upgrades.log >/dev/null 2>&1 ; do
-                    sleep 1
-                    done
-                fi
-            EOT
-        ]
-    }
-    provisioner "remote-exec" {
-        inline = [
-        
+            "apt-get update",
+            "DEBIAN_FRONTEND=noninteractive apt-get -qq install musl-tools build-essential -y",
             "git clone https://github.com/${var.repo_owner}/safe_network -q",
             "cd safe_network",
             "git checkout ${var.commit_hash}",
             "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q --default-host x86_64-unknown-linux-gnu --default-toolchain stable --profile minimal -y",
             ". $HOME/.cargo/env",
             "rustup target add x86_64-unknown-linux-musl",
-            "apt -qq install musl-tools -y ",
             "cargo -q build --release --target=x86_64-unknown-linux-musl",
         ]
     }
