28609d0f0b9f35710272d699ced1d3d7e47a482b
Josh Wilson <joshuef@gmail.com>
2022-07-26 12:40:49 +0200

    chore: small fixes for client droplet scripts

--- scripts/loop_client_tests.sh
+++ scripts/loop_client_tests.sh
@@ -6,27 +6,22 @@ trap cleanup SIGINT SIGTERM ERR EXIT
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-LOOP_COUNT=${1:-5}
-
 cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
   # script cleanup here
 }
 
-run_test_loop() {
 
-  cd safe_network/sn_client
+cd safe_network/sn_client
 
-  # run LOOP_COUNT times, increment counter once per iter
-  for i in {0..$LOOP_COUNT..1}
-  do
-    echo "iteration $i, time:"
-    RUST_LOG=sn_client cargo test --release -- --skip spent > ../../test-$i.log || true
-  done
+# run 10 times, increment counter once per iter
+for i in {0..10..1}
+do
+  echo "iteration $i, time:"
+  RUST_LOG=sn_client cargo test --release -- --skip spent > ../../test-$i.log || true
+done
 
-  cd -
-}
+cd -
 
-nohup run_test_loop &
 
 cleanup
