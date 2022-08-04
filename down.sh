845c3e477cd15d8267a2981fc4d94109d51774e7
Chris O'Neil <chriso83@protonmail.com>
2022-08-04 22:15:40 +0100

    feat(debug): script for pulling logs
    Based on the `logs` script, this one is very similar, it just operates in a synchronous way rather
    than launching rsync in the background.
    
    It's intended to be used mainly for debugging the nightly, so we will want to wait till all the logs
    have been downloaded before moving on to the next step.

--- down.sh
+++ down.sh
@@ -13,5 +13,4 @@ AUTO_APPROVE=${2}
 terraform destroy \
   -var "do_token=${DO_PAT}" \
   -var "pvt_key=${1}" \
-  --parallelism 15 ${AUTO_APPROVE} && \
-  rm ${TESTNET_CHANNEL}-ip-list || true
+  --parallelism 15 ${AUTO_APPROVE}
