dd1ca4da429a812c1e178345430752cbcd04e346
Chris O'Neil <chriso83@protonmail.com>
2022-08-04 22:15:40 +0100

    feat(debug): script for pulling logs
    Based on the `logs` script, this one is very similar, it just operates in a synchronous way rather
    than launching rsync in the background.
    
    It's intended to be used mainly for debugging the nightly, so we will want to wait till all the logs
    have been downloaded before moving on to the next step.

---
+++ scripts/logs-sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is mainly intended to be used for debugging the nightly run.
+# It's expecting the ip list file to be in the same directory the script is running from.
+
+set -e
+
+testnet_channel="$1"
+if [[ -z "$testnet_channel" ]]; then
+  echo "The name of the testnet must be provided"
+  exit 1
+fi
+
+mkdir -p logs/${testnet_channel}
+
+cat ${testnet_channel}-ip-list | while read line; do
+  name=$(echo $line | awk '{print $1}')
+  ip=$(echo $line | awk '{print $2}')
+  echo "Getting $name logs from $ip"
+  rsync -v -r root@${ip}:~/logs logs/${testnet_channel}/${name}___${ip}
+done
