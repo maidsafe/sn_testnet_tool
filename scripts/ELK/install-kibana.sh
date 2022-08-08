4d9ec18041304fab38d0d2811da8ed43d728397f
yoga07 <yogeshwar_1997@hotmail.com>
2022-08-08 18:21:43 +0530

    chore(elk): furnish scripts to auto-start elastic and kibana

--- scripts/ELK/install-kibana.sh
+++ scripts/ELK/install-kibana.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ELASTIC-IP=${1}
+
 # Add gpg keys and install Kibana
 echo "Setting up gpg keys"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 
 echo "Installing required libs"
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https && sudo apt-get install rpl
 
 # Write to source
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
@@ -13,3 +15,19 @@ echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://arti
 # Update and install Kibana
 echo "Installing kibana"
 sudo apt-get update && sudo apt-get install kibana
+
+# Write customised config over default config
+echo "Pull config from S3"
+wget https://safe-testnet-tool.s3.eu-west-2.amazonaws.com/ELK/templates/kibana.yml -O /etc/kibana/kibana.yml
+
+# Get public IP
+echo "Fetching Public IP"
+ip=$(curl ident.me)
+
+# Replace the IP placeholder with host's public IP
+echo "Replacing placeholders with Public IPs of Elastic"
+rpl "<ELASTIC-MACHINE-PUBLIC-IP>" "${ELASTIC-IP}" /etc/kibana/kibana.yml
+rpl "<KIBANA-MACHINE-PUBLIC-IP>" "${ip}" /etc/kibana/kibana.yml
+
+# Start the service
+systemctl start kibana
