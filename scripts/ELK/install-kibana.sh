0f8cb58b7476a34e008007e5a8dda16414b15fe4
yoga07 <yogeshwar_1997@hotmail.com>
2022-08-09 13:43:09 +0530

    chore(elk): default to accept apt-get installations

--- scripts/ELK/install-kibana.sh
+++ scripts/ELK/install-kibana.sh
@@ -7,14 +7,16 @@ echo "Setting up gpg keys"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 
 echo "Installing required libs"
-sudo apt-get install apt-transport-https && sudo apt-get install rpl
+sudo apt-get install apt-transport-https -y > /dev/null 2>&1
+sudo apt-get install rpl -y > /dev/null 2>&1
 
 # Write to source
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 # Update and install Kibana
 echo "Installing kibana"
-sudo apt-get update && sudo apt-get install kibana
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install kibana -y > /dev/null 2>&1
 
 # Write customised config over default config
 echo "Pull config from S3"
