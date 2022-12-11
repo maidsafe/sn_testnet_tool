25c9642d8a8c432a8f4c38ff92f5c4873fb8e8b3
Josh Wilson <joshuef@gmail.com>
2022-12-11 06:52:39 +0100

    cleanup scripts folder, add docs for remaining

--- scripts/download-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Downloading node binary to nodes"
-node_version="latest"
-node_url="https://sn-node.s3.eu-west-2.amazonaws.com/sn_node-${node_version}-x86_64-unknown-linux-musl.tar.gz"
-wget ${node_url}
-tar xf sn_node-${node_version}-x86_64-unknown-linux-musl.tar.gz
-echo "Node bin downloaded"
