dde2c1c355a52ecc90e4d285b8e7ad9b2102b4b2
Chris O'Neil <chriso83@protonmail.com>
2023-02-17 00:53:26 +0000

    feat: create a client machine for running tests
    Use the `enable_client` variable to control the creation of a client machine from which you can run
    a set of tests against the testnet. This machine uses the same AMI and type as the one we use for
    our self hosted CI jobs.
    
    The Justfile targets were refactored to smaller functions to reduce duplication.

--- aws/terraform/variables.tf
+++ aws/terraform/variables.tf
@@ -24,3 +24,19 @@ variable "node_count" {
   default = 30
   description = "The number of EC2 instances to launch for the nodes"
 }
+
+variable "enable_client" {
+  type = bool
+  default = false
+  description = "Controls provisioning for a machine for running client tests"
+}
+
+variable "client_instance_type" {
+  default = "c5.9xlarge"
+  description = "Type of the client instance (typically bigger than a node machine)"
+}
+
+variable "client_ami_id" {
+  default = "ami-00632730219704074"
+  description = "Client AMI ID which is the same as the build agent"
+}
