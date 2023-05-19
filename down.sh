d8282b1831aa69fc179a739ea393672fab418205
Chris O'Neil <chriso83@protonmail.com>
2023-05-19 16:48:41 +0100

    feat: use `up` and `down` wrappers
    The definition of the container was changed such that arbitrary commands can be provided to run in
    the container's Bash shell. This removes the requirement to use the shell inside the container for
    running `just` commands. It seemed appropriate to rename the `runner.sh` script to `run.sh`, since
    it now runs arbitrary commands.
    
    In addition to `run.sh`, `up.sh` and `down.sh` scripts were provided, which can be used as simple
    wrappers for bringing up or tearing down a testnet without using any `just` commands directly.
    
    The `run.sh` script should still be used for doing things like retrieving logs.
    
    The container definition was also modified to revert back to using the single `RUN` instruction, as
    per best practice.

---
+++ down.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+env="$1"
+if [[ -z "$env" ]]; then
+  echo "A name for the environment must be provided"
+  exit 1
+fi
+
+provider="$2"
+if [[ -z "$provider" ]]; then
+  echo "The cloud provider must be specified"
+  echo "Valid values are 'aws' or 'digital-ocean'"
+  exit 1
+fi
+
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+docker run --rm --interactive --tty \
+  --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION \
+  --env SSH_KEY_NAME --env DIGITALOCEAN_TOKEN --env DO_API_TOKEN \
+  --env SN_TESTNET_DEV_SUBNET_ID --env SN_TESTNET_DEV_SECURITY_GROUP_ID \
+  --volume $HOME/.ansible:/home/runner/.ansible \
+  --volume $HOME/.ssh:/home/runner/.ssh \
+  --volume $(pwd):/home/runner/sn_testnet_tool \
+  sn_testnet_tool:latest just clean $env $provider
